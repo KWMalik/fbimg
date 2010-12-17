58afc0c547fe3c0251c20e58baccddd74a0204ec
Andrew Tinits <atinits7d5@gmail.com>
2010-12-17 14:57:32 -0800

    added some errors

--- fbimg.js
+++ fbimg.js
@@ -4,19 +4,24 @@ $(document).ready(function(){
 });
 function refresh(searchterm){
 	var limit = 250;
-	$.getJSON("http://graph.facebook.com/search?q="+encodeURI(searchterm)+"&type=post&limit="+limit+"&callback=?",function(json){
-        if (json.data.length == 0) {
-			error.add("No Results!");
-			searchEnd();
-		}
-		$.each(json.data, function(i, e){
-			if(json.data[i].picture != null){
-				$("#test").append("<img id='photo"+i+"' />");
-				imagesToLoad++;
-				$("#photo"+i).attr("src", json.data[i].picture).load(imageLoaded);
-			}
-		});					
-	});
+	$.getJSON("http://graph.facebook.com/search?callback=?",{
+            q:          searchterm,
+            type:       "post",
+            limit:      limit
+        },function(json){
+            if (!json || !json.data || json.data.length == 0) {
+			    error.add("No Results!");
+			    searchEnd();
+		    }
+		    $.each(json.data, function(i, e){
+			    if(json.data[i].picture != null){
+				    $("#test").append("<img id='photo"+i+"' />");
+				    imagesToLoad++;
+				    $("#photo"+i).attr("src", json.data[i].picture).load(imageLoaded);
+			    }
+		    });					
+	    }
+    );
 }
 function imageLoaded(){
 	imagesToLoad--;
