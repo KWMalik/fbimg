cd74908d984076bc54ef78594e65b6e517d7593f
Andrew Tinits <atinits7d5@gmail.com>
2010-12-17 14:07:41 -0800

    Added checking for null, and handling no results

--- fbimg.js
+++ fbimg.js
@@ -1,17 +1,15 @@
 var imagesToLoad = 0;
-var result = null;
 $(document).ready(function(){
 	//$("#searchbox").bind('blur', search);
-	$("#searchbox").keypress(function(e){ if(e.which == 13) search(); });
+	$("#searchbox").keypress(function(e){ if(e.which == 13) search($("#searchbox").val()); });
 });
 function refresh(searchterm){
 	var limit = 250;
 	$.getJSON("http://graph.facebook.com/search?q="+encodeURI(searchterm)+"&type=post&limit="+limit+"&callback=?",function(json){
-		result = json;
-		if (json.data.length == 0) {
-			$("#test").append("No Results!");
+        if (json.data.length == 0) {
+			$("#error").html("No Results!");
 			searchEnd();
-		}	
+		}
 		$.each(json.data, function(i, e){
 			if(json.data[i].picture != null){
 				$("#test").append("<img id='photo"+i+"' />");
@@ -27,17 +25,17 @@ function imageLoaded(){
 		searchEnd();
 	}
 }
-function search(){
+function search(query){
+    if (query == "") return false;
 	$("#test").fadeOut(function(){
 		$("#test").html("");
 	});
 	$("#search").fadeOut();
 	$("#load").fadeIn();
-	refresh($("#searchbox").val());
+	refresh(query);
 	$("#test").fadeIn(function(){});	
 }
 function searchEnd(){
 		$("#load").fadeOut();
 		$("#search").fadeIn();
 }
-
